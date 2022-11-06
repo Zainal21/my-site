6aa745f60d712a21e89d0ac97f376be2b428bd4a
Zainal21 <Zainalarifin080718@gmail.com>
2022-11-06 22:46:01 +0700

    fix : font size music section

--- components/modules/recent-playlist.jsx
+++ components/modules/recent-playlist.jsx
@@ -9,7 +9,7 @@ export default function RecentPlaylist() {
   // console.log("top track >>", topTrack);
   return (
     <Section className="space-y-9 pt-10">
-      <h1 className="text-gray-100 text-2xl text-xl  text-medium font-bold mb-8  mt-8 underline">
+      <h1 className="text-gray-100 lg:text-2xl text-lg text-medium font-bold mb-8  mt-8 underline">
         My Music Activity
       </h1>
       <SpotifyCurrentlyPlaying />

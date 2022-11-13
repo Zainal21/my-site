c84c48914211b2667e28e5d8e60959a35f921bf4
Zainal21 <Zainalarifin080718@gmail.com>
2022-11-13 14:16:06 +0700

    chore : hide playlist card component

--- components/modules/recent-playlist.jsx
+++ components/modules/recent-playlist.jsx
@@ -1,16 +1,21 @@
 import Section from "../ui/section";
 import { SpotifyCurrentlyPlaying } from "../base/home/sportify-current-playing";
-import { useSpotifyPlaylist } from "@/lib/playlist";
+// import PlaylistCard from "../base/home/playlist-card";
+// import { useSpotifyPlaylist } from "@/lib/playlist";
 
 export default function RecentPlaylist() {
-  const data = useSpotifyPlaylist();
-  console.log(data);
+  // const data = useSpotifyPlaylist();
+  // console.log(data);
   return (
     <Section className="space-y-9 pt-10">
       <h1 className="text-gray-100 lg:text-2xl text-lg text-medium font-bold mb-8  mt-8 underline">
         My Music Activity
       </h1>
       <SpotifyCurrentlyPlaying />
+      {/* <h1 className="text-gray-100 lg:text-2xl text-lg text-medium font-bold mb-8  mt-8 underline">
+        My Music Playlist
+      </h1>
+      <PlaylistCard /> */}
     </Section>
   );
 }

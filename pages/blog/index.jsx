85b988c5ce5e475b7f70ded7561d8519b9908aca
Zainal21 <Zainalarifin080718@gmail.com>
2023-01-01 21:55:52 +0700

    fix : retrieve mdx file serialize data type

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -8,17 +8,6 @@ import { getPosts } from "../../lib/utils";
 import Link from "next/link";
 
 const indexPage = ({ posts }) => {
-  // const [filteredBlog, setFilteredBlog] = useState(posts);
-  // const [currentPageIndex, setCurrentPageIndex] = useState(1);
-
-  // const handleLoadMoreBlogItem = async () => {
-  //   const response = await fetch(`/api/posts?page=${currentPageIndex + 1}`);
-  //   const posts = await response.json();
-
-  //   setFilteredBlog((_posts) => [..._posts, ...posts]);
-  //   setCurrentPageIndex((_pageIndex) => _pageIndex + 1);
-  // };
-
   return (
     <Fragment>
       <MetaTag title="Blog | Muhamadzain.dev" />
@@ -36,30 +25,23 @@ const indexPage = ({ posts }) => {
               <div className="pt-16 lg:pt-20 relative">
                 <div className="pt-2 lg:pt-4">
                   <ul className="mx-4">
-                    {[] ||
-                      posts.map((post, index) => (
-                        <div
-                          key={index}
-                          className="border border-2 border-gray-700 rounded-md p-6 m-4"
-                        >
-                          <h3 className="text-left  py-3 text-2xl font-bold">
-                            <Link href={`/blog/${post.slug}`}>
-                              {post.data.title}
-                            </Link>
-                          </h3>
-                          <p>{post.data.excerpt}</p>
-                          <p className="text-gray-200 mt-2 hover:underline hover:underline-offset-auto">
-                            <Link href={`/blog/${post.slug}`}>Read more</Link>
-                          </p>
-                        </div>
-                      ))}
+                    {(posts || []).map((post, index) => (
+                      <div
+                        key={index}
+                        className="border border-2 border-gray-700 rounded-md p-6 m-4"
+                      >
+                        <h3 className="text-left  py-3 text-2xl font-bold">
+                          <Link href={`/blog/${post.slug}`}>
+                            {post.data.title}
+                          </Link>
+                        </h3>
+                        <p>{post.data.excerpt}</p>
+                        <p className="text-gray-200 mt-2 hover:underline hover:underline-offset-auto">
+                          <Link href={`/blog/${post.slug}`}>Read more</Link>
+                        </p>
+                      </div>
+                    ))}
                   </ul>
-                  {/* <button
-                    className="text-center m-auto block text-xl font-bold p-4 text-gray-300  border-gray-500 rounded"
-                    onClick={handleLoadMoreBlogItem}
-                  >
-                    Load More
-                  </button> */}
                 </div>
               </div>
             </div>
@@ -72,11 +54,11 @@ const indexPage = ({ posts }) => {
 
 export default indexPage;
 
-// export const getStaticProps = () => {
-//   const posts = getPosts();
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// };
+export const getStaticProps = () => {
+  const posts = getPosts(1);
+  return {
+    props: {
+      posts,
+    },
+  };
+};

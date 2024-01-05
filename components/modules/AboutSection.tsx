4f30ec90ebd6a5d71340e18a3886545cb31edae7
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-01-05 13:54:15 +0700

    fix : lint html entity code disable

--- components/modules/AboutSection.tsx
+++ components/modules/AboutSection.tsx
@@ -1,19 +1,17 @@
 import { Heading, Text, TagLink } from "@/components/design-system";
 
 const AboutSection = () => {
-  const TEXT_MARGIN_BOTTOM = 1.5;
-
   return (
     <section className="flex flex-col mb-12">
       <Heading>About 💭</Heading>
 
-      <Text style={{ marginBottom: `${TEXT_MARGIN_BOTTOM}rem` }}>
-        I&apos;m a {new Date().getFullYear() - 2003} a passionate individual
+      <Text style={{ marginBottom: `1.5rem` }}>
+        I'm a {new Date().getFullYear() - 2003} a passionate individual
         exploring the realms of technology, with a specific interest in web and
         mobile application development.
       </Text>
 
-      <Text style={{ marginBottom: `${TEXT_MARGIN_BOTTOM}rem` }}>
+      <Text style={{ marginBottom: `1.5rem` }}>
         With over 3 years of hands-on experience in programming, I have honed my
         skills in utilizing frameworks and libraries such as Codeigniter,
         Laravel, Vue JS, React JS, Kotlin, Express JS, Nest JS, and Go Fiber,

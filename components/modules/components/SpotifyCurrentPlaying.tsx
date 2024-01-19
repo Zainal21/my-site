9f0814e0d82876494091bd9561f595cf45f8a631
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-01-19 11:28:32 +0700

    feat: change image component to next image

--- components/modules/components/SpotifyCurrentPlaying.tsx
+++ components/modules/components/SpotifyCurrentPlaying.tsx
@@ -1,13 +1,14 @@
 import { useCurrentlyPlaying } from "@/hooks/useSpotifyCurrentlyPlaying";
 import { PlayCircle } from "react-feather";
 import cn from "classnames";
+import Image from "next/image";
 
 export default function SpotifyCurrentlyPlaying(): React.JSX.Element {
   const { data } = useCurrentlyPlaying();
   return (
     <div className="w-[90%] md:w-[40rem]  flex items-center relative space-x-4 bg-[#10161a] dark:bg-[#10161a] px-5 rounded-md shadow-single border border-teal-100 dark:border-teal-900">
       {data?.albumImageUrl ? (
-        <img
+        <Image
           className="w-12 h-10 rounded-sm"
           src={data?.albumImageUrl || ""}
           alt={data?.artist}

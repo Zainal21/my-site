e310be4efe94948101bec32fabf916a5eca54862
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-01-28 14:05:09 +0700

    chore: clean up code

--- layouts/HomePageLayout.tsx
+++ layouts/HomePageLayout.tsx
@@ -5,7 +5,7 @@ export const HomePageLayout: React.FC<{
 }> = ({ children }) => {
   return (
     <div className="flex flex-col items-center justify-center h-full scrollbar-thumb-gray-700 scrollbar-track-gray-800 scrollbar-thumb-rounded-full">
-      <div className="flex flex-col h-full w-[90%] md:w-[40rem] mt-12">
+      <div className="flex flex-col h-full w-[80%] md:w-[40rem] mt-12">
         <NavbarSection />
         <main className="flex md:flex-row md:flex-start flex-col-reverse items-start md:items-center w-full justify-between mb-12 sm:mt-16 mt-20">
           {children}

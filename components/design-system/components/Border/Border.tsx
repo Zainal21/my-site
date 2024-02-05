a7d94cb52b15355b06090d26fbb367fef133f13b
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-02-05 12:56:43 +0700

    feat: change border shadow neon

--- components/design-system/components/Border/Border.tsx
+++ components/design-system/components/Border/Border.tsx
@@ -4,7 +4,7 @@ interface BorderProps {
 
 const Border: React.FC<BorderProps> = ({ children }) => {
   return (
-    <div className="flex w-fit  border-4 duration-300 border-teal-200 dark:border-teal-600 rounded-full  shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+    <div className="flex w-fit  border-4 duration-300 border-teal-200 dark:border-teal-200 rounded-full shadow-[0_0_1px_#fff,inset_0_0_2px_#fff,0_0_1px_#99f6e3,0_0_10px_#99f6e3,0_0_30px_#99f6e3]">
       {children}
     </div>
   );

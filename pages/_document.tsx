2479283ba8afd9bd95b11a66e60d30ae5c796cd8
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-03-01 09:17:32 +0700

    feat: add google site verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html className="dark" lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="yX9XVvYh0SFOZ13C28ImKEtMsl8j8HCDCvfeNJGz-Tg"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
